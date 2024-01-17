5648a708d0cbb07a82786d88a1256af085443f4c
dudeslav1ch <dudeslav1ch@gmail.com>
2024-01-17 23:19:14 +0300

    hw-1

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,15 @@
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        """Экземпляр инициализирует id канала. Дальше все данные будут подтягиваться по API."""
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        youtube = build('youtube', 'v3', developerKey='AIzaSyCMfJdJiQ2ONFN-FX0w0VLu6gpuLyHcFj4')
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(channel)
